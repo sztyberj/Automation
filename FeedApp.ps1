19d846eb1a6f3f02018fca72e671c68c71c02e57
Jakub Sztyber <54943867+sztyberj@users.noreply.github.com>
2021-07-14 12:07:21 +0200

    Add files via upload
    Next script to work.
    The script copies files from Data wareHouse resources to a temporary folder, rename the file and move all files to the appropriate directory.

---
+++ FeedApp.ps1
@@ -0,0 +1,39 @@
+#lista lokalizacji
+$dir_rachunki = "File dir"
+$dir_powiazania = "File dir"
+$dir_klienci = "File dir"
+$dir_transakcje = "File dir"
+
+$dir_HD = "DatawareHouse resource"
+
+
+#tablica lokalizacji
+$directory = @($dir_klienci, $dir_transakcje, $dir_transakcje, $dir_rachunki, $dir_powiazania)
+
+
+#tablica plików
+$items = @($klienci, $trans_pxt, $trans_txt, $rachunki, $powiazania)
+
+for ($i=0; $i -lt 5; $i++) {
+    $items[$i] = Get-ChildItem -Path $dir_HD | Sort-Object CreationTime -Descending | Select-Object -ExpandProperty name | Select-Object -Skip $i | Select -First 1 
+    }
+
+$items
+
+for ($i=0; $i -lt 5; $i++) {
+    Copy-Item ($dir_HD + $items[$i]) -Destination C:\PlikiTymczasowe_z_Prod\
+    }
+
+Get-ChildItem 'C:\TempFolder' | Rename-Item -NewName { $_.BaseName.Split('_')[0] + $_.Extension }
+
+for ($i=0; $i -lt 5; $i++) {
+    $items[$i] = Get-ChildItem -Path "C:\PlikiTymczasowe_z_Prod\" | Sort-Object CreationTime -Descending | Select-Object -ExpandProperty name | Select-Object -Skip $i | Select -First 1 
+    }
+
+
+
+for ($j=0; $j -lt 5; $j++) {
+Move-Item -Path ("C:\TempFolder" + $items[$j]) -Destination $directory[$j]
+} 
+
+
